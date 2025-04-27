aec2116780cbe81c14da4dfc94646e7b21ac01fb
naroso_o <naroso1751@naver.com>
2025-04-27 19:23:12 +0900

    chore: add type definitions

--- src/features/post/model/useTags.ts
+++ src/features/post/model/useTags.ts
@@ -1,9 +1,11 @@
 import { useState } from "react"
 
+type Tag = { url: string; slug: string }
+
 const useTags = () => {
   const queryParams = new URLSearchParams(location.search)
 
-  const [tags, setTags] = useState([])
+  const [tags, setTags] = useState<Tag[]>([])
   const [selectedTag, setSelectedTag] = useState(queryParams.get("tag") || "")
 
   /** 태그 가져오기 */
@@ -18,7 +20,7 @@ const useTags = () => {
   }
 
   /** 태그 선택하기 */
-  const selectTag = (tag) => {
+  const selectTag = (tag: string) => {
     setSelectedTag(tag)
   }
 
